f2ce1fa268094c1f2cf8d777460b0f57fa48acba
Reginald Suh <reginald.suh@universe.com>
2017-10-09 23:12:34 -0400

    first commit

---
+++ constants/index.js
@@ -0,0 +1,12 @@
+const API_KEY = '1f20a62a6fd01d5bdbe088a3fa8e6510';
+
+const COURSE_URL   = 'https://api.uwaterloo.ca/v2/terms/';
+const TERM_URL     = 'https://api.uwaterloo.ca/v2/terms/list.json';
+const SUBJECTS_URL = 'https://api.uwaterloo.ca/v2/codes/subjects.json';
+
+module.exports = {
+  API_KEY,
+  COURSE_URL,
+  TERM_URL,
+  SUBJECTS_URL
+};
---
+++ routes/users.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+var request = require('request');
+
+var { API_KEY, TERM_URL } = require('../constants/index');
+
+/* GET users listing using request. */
+router.get('/', function(req, res, next) {
+  request({
+    uri: TERM_URL,
+    qs: {
+      key: API_KEY
+    }
+  }).pipe(res);
+});
+
+module.exports = router;
